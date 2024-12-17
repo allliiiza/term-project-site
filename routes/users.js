ef26a2dbd2c3eb9822e7525e9f0bd7ebc51b0d4b
Diya Tamrakar <112214017+diyatamrakar@users.noreply.github.com>
2024-12-17 09:59:38 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -54,4 +54,14 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Logout error:', err);
+            return res.status(500).send('Error logging out');
+        }
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
