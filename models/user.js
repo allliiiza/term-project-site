e83e738ade77e5e4b5f78b7f98eee7502dddcc65
allliiiza <142860510+allliiiza@users.noreply.github.com>
2024-12-17 16:11:12 -0800

    updating user.js

--- models/user.js
+++ models/user.js
@@ -1,45 +1,9 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    password: { 
-        type: String, 
-        required: true,
-        unique: true
-    },
-    favorites: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Product' 
-    }],
-    cart: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product'
-        },
-        quantity: {
-            type: Number,
-            default: 1
-        }
-    }],
-    purchaseHistory: [{
-        products: [{
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Product'
-            },
-            quantity: Number,
-            price: Number
-        }],
-        date: {
-            type: Date,
-            default: Date.now
-        },
-        totalAmount: Number
-    }]
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', UserSchema);
